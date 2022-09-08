8df7e26a41c8d5b5aca06c0504e8a14d9ec14f23
Python For Java Devs <python4javadevs@proton.me>
2022-09-07 19:44:14 -0500

    Apply Apache 2.0 Copyright comment to all Java source files

--- switch-java/src/main/java/com/python4javadevs/learn/switchdemo/SwitchDemo.java
+++ switch-java/src/main/java/com/python4javadevs/learn/switchdemo/SwitchDemo.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 J Steven Perry
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.python4javadevs.learn.switchdemo;
 
 import java.util.Optional;
@@ -6,6 +22,9 @@ import static com.python4javadevs.learn.switchdemo.Constants.HOLY_GRAIL_TITLE;
 import static com.python4javadevs.learn.switchdemo.Constants.LIFE_OF_BRIAN_TITLE;
 import static com.python4javadevs.learn.switchdemo.Constants.MEANING_OF_LIFE_TITLE;
 
+/**
+ * Demonstrates a few ways to use the Java switch statement
+ */
 public class SwitchDemo {
 
     /**
--- switch-java/src/test/java/com/python4javadevs/learn/switchdemo/SwitchDemoTest.java
+++ switch-java/src/test/java/com/python4javadevs/learn/switchdemo/SwitchDemoTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 J Steven Perry
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.python4javadevs.learn.switchdemo;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +35,10 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * JUnit 5 Tests for SwitchDemo. Because unit testing is a good thing.
+ * Don't let anyone tell you different.
+ */
 @DisplayName("Testing SwitchDemo")
 public class SwitchDemoTest {
 
